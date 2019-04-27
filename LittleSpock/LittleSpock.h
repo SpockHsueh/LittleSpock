85e2ac0a72293372cd0d7616ee9cf4c1e16a0b53
Spock <>
2019-04-28 00:06:50 +0800

    Initial project setup

---
+++ LittleSpock/LittleSpock.h
@@ -0,0 +1,19 @@
+//
+//  LittleSpock.h
+//  LittleSpock
+//
+//  Created by Spock on 2019/4/27.
+//  Copyright © 2019 Spock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LittleSpock.
+FOUNDATION_EXPORT double LittleSpockVersionNumber;
+
+//! Project version string for LittleSpock.
+FOUNDATION_EXPORT const unsigned char LittleSpockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LittleSpock/PublicHeader.h>
+
+
---
+++ LittleSpockExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LittleSpockExamples
+//
+//  Created by Spock on 2019/4/27.
+//  Copyright © 2019 Spock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
