fce92b3541e3c1227d9ecddcf2c5127486ea172d
Spock <>
2019-04-29 23:14:38 +0800

    Added test cases

---
+++ LittleSpock/LittleSpock.swift
@@ -0,0 +1,24 @@
+//
+//  LittleSpock.swift
+//  LittleSpock
+//
+//  Created by Spock on 2019/4/28.
+//  Copyright © 2019 Spock. All rights reserved.
+//
+
+import Foundation
+
+public final class LittleSpock {
+    let name = "LittleSpock"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
+
+
+
--- LittleSpockTests/LittleSpockTests.swift
+++ LittleSpockTests/LittleSpockTests.swift
@@ -11,24 +11,23 @@ import XCTest
 
 class LittleSpockTests: XCTestCase {
 
+    var littleSpock: LittleSpock!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        littleSpock = LittleSpock()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testAdd() {
+        XCTAssertEqual(littleSpock.add(a: 1, b: 1), 2)
     }
-
+    
+    func testSub() {
+        XCTAssertEqual(littleSpock.sub(a: 2, b: 1), 1)
+    }
 }
